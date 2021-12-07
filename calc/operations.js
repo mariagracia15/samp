ddefb29302f298db787687cad21ebea73091b798
mariagracia15 <89884881+mariagracia15@users.noreply.github.com>
2021-12-07 12:42:56 +0800

    Add files via upload

---
+++ calc/operations.js
@@ -0,0 +1,10 @@
+document.getElementById("result").value="hello";
+console.log("Hello");
+function computeSum(){
+    document.getElementById("result").value=parseFloat(document.getElementById("num1").value)+parseFloat(document.getElementById("num2").value);
+
+}
+
+function about(){
+    window.alert("This is programmed by: \nMaria Gracia");
+}
